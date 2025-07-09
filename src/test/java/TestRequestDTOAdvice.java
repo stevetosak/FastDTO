4c37997a07f2e2e1dbe61951b21acd43813720e9
viktor <viktorhristovski629@gmail.com>
2025-07-10 00:15:34 +0200

    feat: Migrated package manager from gradle to maven

--- src/test/java/TestRequestDTOAdvice.java
+++ src/test/java/TestRequestDTOAdvice.java
@@ -1,11 +1,13 @@
 import com.vitosak.Config;
 import com.vitosak.RequestDTOClassComponent;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringJUnitConfig(Config.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class TestRequestDTOAdvice {
     @Autowired
     RequestDTOClassComponent requestDTOClassComponent;
@@ -29,7 +32,5 @@ public class TestRequestDTOAdvice {
               "age": 30
             }
             """;
-
-
     }
 }
